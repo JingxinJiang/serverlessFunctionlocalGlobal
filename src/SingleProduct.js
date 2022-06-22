5c88c03bb2f002dd75fc3b055815e3c7fe6cf5fa
JingxinJiang <jiajingx@sheridancollege.ca>
2022-06-22 13:51:09 -0400

    formatChange

--- src/SingleProduct.js
+++ src/SingleProduct.js
@@ -33,31 +33,23 @@ const SingleProduct = () => {
     const {name,desc,price,image}=products.fields
     console.log(price)
   return (
-    <section className='section section-center'>
-        
-               
-        <NavLink to='/' className='link'>back home</NavLink>
-        
+    <section className='section section-center'>            
+        <NavLink to='/' className='link'>back home</NavLink>        
         <div >
-            {/* */}
+            
             <div className="title">
                 <h2>{name}</h2>
                 <div className="title-underline"></div>
-                <article className='single-product'>
-                     <img className='single-product-img' src={image&&image[0].url} alt={name}/>
-                     <div>
-                        <h5>{name}</h5>
-                        <h5 className='price'>${price}</h5>
-                        <p>{desc}</p>
-                    </div>
-                </article>
-                
-                
             </div>
-            </div>
-            
-    
-       
+            <article className='single-product'>
+                    <img className='single-product-img' src={image&&image[0].url} alt={name}/>
+                    <div>
+                    <h5 >{name}</h5>
+                    <h5 className='single-product-price'>${price}</h5>
+                    <p>{desc}</p>
+                </div>
+            </article>
+        </div>          
     </section>
   )
 }
